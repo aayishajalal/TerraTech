1e75ecf64b1f314eca21607f679d6db50ff0b3c7
Aayisha Noora Jalaludeen <112081762+aayishajalal@users.noreply.github.com>
2024-02-15 20:31:41 +0530

    Add files via upload

---
+++ app/Card/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <div>
+        <div className="border-8 w-96 min-h-96"></div>
+      </div>
+    </>
+  );
+};
+
+export default page;
